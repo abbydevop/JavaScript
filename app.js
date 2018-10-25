beee394a53df36c314d230a138e7225ce273f31f
abbydevop <33470524+abbydevop@users.noreply.github.com>
2018-10-24 23:19:37 -0400

    Store Filter

---
+++ app.js
@@ -0,0 +1,85 @@
+// filter btns
+(function(){
+const filterBtn=document.querySelectorAll('.filter-btn');
+// console.log(filterBtn);
+
+filterBtn.forEach(function(btn){
+    btn.addEventListener('click',function(event){
+        event.preventDefault();
+        const value=event.target.dataset.filter;
+        // console.log(value);
+        const items=document.querySelectorAll('.store-item');
+        // console.log(items);
+
+        items.forEach(function(item){
+            if(value==='all'){
+                item.style.display='block';
+            }else{
+                if(item.classList.contains(value)){
+                    item.style.display='block';
+                }else{
+                    item.style.display='none';
+                }
+            }
+        })
+    });
+})
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// search box
+(function(){
+const search=document.getElementById('search-item');
+
+search.addEventListener('keyup',function(){
+    let value=search.value.toLowerCase().trim();
+    // console.log(value);
+    const items=document.querySelectorAll('.store-item');
+
+    items.forEach(function(item){
+        let type=item.dataset.item;
+        // if(type.includes(value)){
+        //     item.style.display='block';
+        // }
+        // else{
+        //     item.style.display='none';
+        // }
+
+        let length=value.length;
+        let match=type.slice(0,length);
+        if(value==match){
+            item.style.display='block';
+        }
+        else{
+            item.style.display='none';
+        }
+
+    })
+
+
+
+
+})
+
+
+
+
+})();
